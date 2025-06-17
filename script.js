432064b1fdd304eeddae2ac556bbf4fb012255ed
Enzo Weimann <enzoweimann@gmail.com>
2025-06-17 13:53:23 -0300

    Add budget app

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict"
 
-let proyectosArr = ["Person Management with Spring", "Random quote generator", "Palindrome Checker with JS", "Showpping cart with JS"];
-let urlArr = ["https://github.com/EnzoWeimann/People-Management-with-Spring", "https://codepen.io/Endage/pen/BagBLEp", "https://codepen.io/Endage/pen/OPPdJjW", "https://codepen.io/Endage/pen/KwwbvOd"];
-let srcArr = ["/imagenes/Control_usuarios_index.png", "./imagenes/random_quote.png", "./imagenes/palindrome.png", "./imagenes/shopping_cart.png"];
+let proyectosArr = ["Person Management with Spring", "Budget app with Angular", "Random quote generator", "Palindrome Checker with JS", "Showpping cart with JS"];
+let urlArr = ["https://github.com/EnzoWeimann/People-Management-with-Spring", "https://github.com/EnzoWeimann/AppBudget", "https://codepen.io/Endage/pen/BagBLEp", "https://codepen.io/Endage/pen/OPPdJjW", "https://codepen.io/Endage/pen/KwwbvOd"];
+let srcArr = ["/imagenes/Control_usuarios_index.png", "/imagenes/image1.png", "./imagenes/random_quote.png", "./imagenes/palindrome.png", "./imagenes/shopping_cart.png"];
 
 let certificadosArr = ["SCRUM certificate", "Responsive web design certificate", "JavaScript certificate"];
 let certificadosUrl = ["https://www.scrumstudy.com/certification/verify?type=SFC&number=1054182", "https://freecodecamp.org/certification/EnzoW/responsive-web-design", "https://www.freecodecamp.org/certification/EnzoW/javascript-algorithms-and-data-structures-v8"];
